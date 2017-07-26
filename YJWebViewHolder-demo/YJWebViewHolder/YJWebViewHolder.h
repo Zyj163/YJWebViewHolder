40da411614514b7b9affad0339fde06884c8eaf0
ddn <zhangyongjun@pj-l.com>
2017-07-26 16:04:44 +0800

    xx

---
+++ YJWebViewHolder-demo/YJWebViewHolder/YJWebViewHolder.h
@@ -0,0 +1,19 @@
+//
+//  YJWebViewHolder.h
+//  YJWebViewHolder
+//
+//  Created by ddn on 2017/7/26.
+//  Copyright © 2017年 张永俊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YJWebViewHolder.
+FOUNDATION_EXPORT double YJWebViewHolderVersionNumber;
+
+//! Project version string for YJWebViewHolder.
+FOUNDATION_EXPORT const unsigned char YJWebViewHolderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YJWebViewHolder/PublicHeader.h>
+
+
--- YJWebViewHolder.podspec
+++ YJWebViewHolder.podspec
@@ -1,7 +1,7 @@
 Pod::Spec.new do |s|
 
 s.name = 'YJWebViewHolder'
-s.version = '1.2.1'
+s.version = '1.2.2'
 s.license = { :type => "MIT", :file => "LICENSE" }
 s.summary = 'easy use WKWebView in Swift'
 
